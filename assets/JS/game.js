23bee23746fe0601ef198f127cd81a342667de91
Katharine C Humble <katharinechumble@gmail.com>
2021-11-12 23:43:42 -0500

    finished game logic

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,31 +1,109 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#startBtn");
+var getScoresBtn = document.querySelector("getScoresBtn");
 var choicesEl = document.querySelector("#questionAnswers");
-var questionsEl = document.querySelector("#questionCard")
+var questionsEl = document.querySelector("#questionCard");
+var correctAnswerEl = document.querySelector("#correctAnswer");
 
 var currentQuestion = 0;
 var time = questions.length * 15;
 var timerId;
 
 function startQuiz() {
-    var introScreenEl = document.getElementById("#introScreen");
-    introScreenEl.setAttribute("class", "hide");
+  var introScreenEl = document.getElementById("#introScreen");
+  introScreenEl.setAttribute("class", "hide");
 
-    questionsEl.removeAttribute("class");
+  questionsEl.removeAttribute("class");
 
-    timerId = setInterval(clockTick, 1000);
-    timerEl.textContent = time;
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
 
-    getQuestion();
+  getQuestion();
 }
 
 function getQuestion() {
-    var currentQuestion = questions[questionList]
+  var currentQuestion = questions[questionList];
 
-    var questionTextEl = document.getElementById("questionPrompt")
-    questionTextEl.textContent = currentQuestion.questionText;
+  var questionTextEl = document.getElementById("questionPrompt");
+  questionTextEl.textContent = currentQuestion.questionText;
 
-    choicesEl.innerHTML = "";
+  choicesEl.innerHTML = "";
 
-    currentQuestion.options
+  currentQuestion.options.forEach(function (option, i) {
+    var optionButton = document.createElement("button");
+    optionButton.setAttribute("class", "option");
+    optionButton.setAttribute("value", option);
+
+    optionButton.textContent = i + 1 + ". " + option;
+
+    optionButton.onclick = answerClick;
+
+    choicesEl.appendChild(optionButton);
+  });
 }
+
+function answerClick() {
+    if (this.value !== questions[questionList].Answer) {
+        time -= 15;
+        if (time < 0) {
+            time = 0;
+        }
+
+        timerEl.textContent = time;
+        correctAnswerEl.textContent = "Incorrect";
+        correctAnswerEl.style.color = "red";
+    } else {
+        correctAnswerEl.textContent = "Correct!";
+        correctAnswerEl.style.color = "green";
+    }
+
+    questionList++;
+
+    if (questionList === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+function quizEnd() {
+    clearInterval(timerId);
+    var quizEndEl = document.getElementById("quizEndScreen");
+
+    quizEndScreen.removeAttribute("class");
+    var highScoreEl = document.getElementById("high-score");
+    highScoreEl.textContent = time;
+
+    questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+function saveScores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    var newScore = {
+        score: time
+    };
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    window.location.href = "scorecard.html";
+}
+
+function getScores(event) {
+    if (event.key === "Enter") {
+        saveScores();
+    }
+}
+
+getScoresBtn.onclick = saveScores;
+
+startBtn.onclick = startQuiz;
